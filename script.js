b2fa30c5d33618862aaf70cf8a83dc82df10e6e8
Chinmay Pendharkar <notthetup@gmail.com>
2018-02-04 13:45:24 +0800

    first kinda working script with tx/rx

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 window.addEventListener('load', () => {
 	console.log("Hello world");
 
-	var button = document.getElementsByTagName('button')[0]
+	var button = document.getElementsByTagName('button')[0];
+	var input = document.getElementsByTagName('input')[0];
 
-	var TI_BASE = "f0000000-0451-4000-b000-000000000000";
+	var txCharacteristic;
+
 	var NORDIC_SERVICE = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
 	var NORDIC_TX = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
 	var NORDIC_RX = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 
 	var filters = [];
-	filters.push({ services: [ 'battery_service' ] });
+	var services = [];
+
+	filters.push({namePrefix: "HT"});
+	services.push(NORDIC_SERVICE);
 
 	function connectionDisconnectCallback(){
 
@@ -23,35 +28,59 @@ window.addEventListener('load', () => {
 
 	}
 
+	function ab2str(buf) {
+	  return String.fromCharCode.apply(null, new Uint8Array(buf));
+	}
+
+	function str2ab(str) {
+	  var buf = new ArrayBuffer(str.length);
+	  var bufView = new Uint8Array(buf);
+	  for (var i=0, strLen=str.length; i<strLen; i++) {
+	    bufView[i] = str.charCodeAt(i);
+	  }
+	  return buf;
+	}
+
+	input.addEventListener("keyup", function(event) {
+		event.preventDefault();
+		if (event.keyCode === 13) {
+			if (txCharacteristic){
+				console.log("HT> Sending "+ input.value);
+				txCharacteristic.writeValue(str2ab(input.value)).then(function() {
+					console.log("HT> Sent");
+				});
+			}
+		}
+	});
+
 	button.addEventListener('click', () => {
 		navigator.bluetooth.requestDevice({
-			acceptAllDevices: true,
-			optionalServices: ['battery_service'] }).then(function(device) {
+			filters: filters,
+		  optionalServices: services}).then(function(device) {
 
 				deviceName = device.name;
-				console.log('BT>  Device Name:       ' + device.name);
-				console.log('BT>  Device ID:         ' + device.id);
+				console.log('HT>  Device Name:       ' + device.name);
+				console.log('HT>  Device ID:         ' + device.id);
 				device.addEventListener('gattserverdisconnected', function() {
-					console.log("BT> Disconnected (gattserverdisconnected)");
+					console.log("HT> Disconnected (gattserverdisconnected)");
 					closeSerial();
 				}, {once:true});
 				return device.gatt.connect();
 			}).then(function(server) {
-				console.log("BT> Connected");
+				console.log("HT> Connected");
 				btServer = server;
 				return server.getPrimaryService(NORDIC_SERVICE);
 			}).then(function(service) {
-				console.log("BT> Got service");
+				console.log("HT> Got service");
 				btService = service;
 				return btService.getCharacteristic(NORDIC_RX);
 			}).then(function (characteristic) {
 				rxCharacteristic = characteristic;
-				console.log("BT> RX characteristic:"+JSON.stringify(rxCharacteristic));
+				console.log("HT> RX characteristic:"+JSON.stringify(rxCharacteristic));
 				rxCharacteristic.addEventListener('characteristicvaluechanged', function(event) {
-		        // In Chrome 50+, a DataView is returned instead of an ArrayBuffer.
 		        var value = event.target.value.buffer;
-		        console.log("BT> RX:"+JSON.stringify(ab2str(value)));
-		        receiveCallback(value);
+		        console.log("HT> " +  event.timeStamp + " RX:"+ab2str(value));
+		        receiveCallback({"timeStamp": event.timeStamp, "value": value});
 		      });
 				return rxCharacteristic.startNotifications();
 			}).then(function() {
@@ -59,19 +88,12 @@ window.addEventListener('load', () => {
 			}).then(function (characteristic) {
 				txCharacteristic = characteristic;
 				console.log("BT> TX characteristic:"+JSON.stringify(txCharacteristic));
-			}).then(function() {
-				txDataQueue = [];
-				txInProgress = false;
-		    Espruino.Core.Serial.setSlowWrite(false, true); // hack - leave throttling up to this implementation
-		    setTimeout(function() {
-		    	openCallback({ portName : deviceName });
-		    }, 500);
-		  }).catch(function(error) {
-		  	console.log('BT> ERROR: ' + error);
-		  	if (connectionDisconnectCallback) {
-		  		connectionDisconnectCallback(undefined);
-		  		connectionDisconnectCallback = undefined;
-		  	}
-		  });
+    	}).catch(function(error) {
+    	console.log('BT> ERROR: ' + error);
+    	if (connectionDisconnectCallback) {
+    		connectionDisconnectCallback(undefined);
+    		connectionDisconnectCallback = undefined;
+    	}
+    });
 		})
 	})
