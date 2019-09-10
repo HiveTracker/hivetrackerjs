1abbab79cc365f9f83dd0e94f299525b0628bf18
Cedric Honnet <honnet@telecom-paristech.org>
2019-09-11 00:48:32 +0100

    Add function to average the valid centroids.

--- hivetracker.js
+++ hivetracker.js
@@ -114,6 +114,23 @@ function TrackerState(messageH, messageV) {
         return this.isValid(0) || this.isValid(1) ||
                this.isValid(2) || this.isValid(3);
     };
+
+    this.averageDirection = function () {
+        var aveDir = new THREE.Vector3();
+
+        if (this.hasSomeValids()) {
+            var validCpt = 0;
+
+            for (var i = 0; i < 4; i++) {
+                if (this.isValid(i)) {
+                    aveDir.add(this.hits[i].direction);
+                    validCpt++;
+                }
+            }
+            aveDir.multiplyScalar(1/validCpt);
+        }
+        return aveDir;
+    }
 }
 
 function SensorHit(timeH, timeV) {
